7897aa1ab8837f5ddf40ce5e4a00367bbdb15a06
Ericksonxd165 <ericksonxd_164@hotmail.com>
2024-11-22 14:22:01 -0400

    Segundo Commit

--- js/mode_toggler.js
+++ js/mode_toggler.js
@@ -1,18 +1,21 @@
 const d = document;
 
-export default function toggler(element1, element2,body,header) {
+export default function toggler(element1, element2,body,header,links) {
   d.addEventListener("click", e => {
     if (e.target.matches(element1)) {
       d.getElementById(element1.slice(1)).classList.toggle("invisible");
       d.getElementById(element2.slice(1)).classList.toggle("invisible");
       d.querySelector(header).classList.toggle("light-topbar")
       d.querySelector(body).classList.toggle("light-mode")
+      d.querySelector(links).classList.toggle("links-lightmode")
+      
     }
     if (e.target.matches(element2)) {
         d.getElementById(element1.slice(1)).classList.toggle("invisible");
         d.getElementById(element2.slice(1)).classList.toggle("invisible");
         d.querySelector(header).classList.toggle("light-topbar")
         d.querySelector(body).classList.toggle("light-mode")
+        d.querySelector(links).classList.toggle("links-lightmode")
       }
   
 
--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const d = document;
 import toggler from "./js/mode_toggler.js";
 
 d.addEventListener("DOMContentLoaded", e => {
-  toggler("#light-mode", "#night-mode","body","header","");
+  toggler("#light-mode", "#night-mode","body","header","#link");
 });
